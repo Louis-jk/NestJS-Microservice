bd805880a45ad623957386a80e18256f50f201e1
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-23 22:57:32 +0900

    modify entities array

--- admin/src/typeorm/config.ts
+++ admin/src/typeorm/config.ts
@@ -1,22 +1,17 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
-import { Product } from 'src/product/product.entity';
 
 dotenv.config();
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  port: 3306,
   host: process.env.DB_HOST,
+  port: 3306,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [Product],
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: true,
-  autoLoadEntities: true,
-  charset: 'utf8mb4',
-  logging: true,
-  keepConnectionAlive: true,
 };
 
 export = config;
