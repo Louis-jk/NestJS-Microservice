563bd09da84c92cc07d5034a5bc60eed653d8381
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-23 22:56:28 +0900

    create project entity

---
+++ admin/src/project/project.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  projectname: string;
+
+  @Column()
+  startdate: string;
+
+  @Column({ nullable: true })
+  enddate: string;
+
+  @Column({ type: 'datetime' })
+  createdAt: Date;
+}
