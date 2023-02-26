bdee84bf503be9e5deb1ebdcb193b9cfc1d387d9
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-26 21:38:43 +0900

    Add checking method whether it's development mode or production mode

---
+++ admin/src/infrastructure/config/typeorm/typeorm.config.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+const isDev = process.env.NODE_ENV === 'local';
+dotenv.config({ path: `./env/${isDev ? 'local' : 'prod'}.env` });
+
+export const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+  synchronize: false,
+};
