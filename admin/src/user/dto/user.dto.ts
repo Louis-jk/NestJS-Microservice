0eca1f4928c429389ae576dbb01e9a523b81b1be
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-26 17:25:05 +0900

    create class 'update user dto'

--- admin/src/user/dto/user.dto.ts
+++ admin/src/user/dto/user.dto.ts
@@ -1,6 +1,9 @@
-export class CreateUserDto {
+import { IUser } from '../interface/user.interface';
+export class CreateUserDto implements IUser {
   username: string;
   email: string;
   password: string;
   phone: string;
 }
+
+export class UpdateUserDto implements Partial<IUser> {}
