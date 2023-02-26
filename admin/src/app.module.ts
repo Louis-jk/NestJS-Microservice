255247ea4b3fe76192cc30b55723ea32ee0f8965
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-26 21:40:22 +0900

    modify of imports partial code

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -3,28 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProJectController } from './project/project.controller';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import * as dotenv from 'dotenv';
 import { LoggerMiddleware } from './middleware/logger.middleware';
+import { config as TypeOrmCofig } from './infrastructure/config/typeorm/typeorm.config';
 
 dotenv.config();
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: 3306,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: false,
-    }),
-    ProjectModule,
-    UserModule,
-  ],
+  imports: [TypeOrmModule.forRoot(TypeOrmCofig), ProjectModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
