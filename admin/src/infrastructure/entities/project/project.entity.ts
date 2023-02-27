f1f2aaaa2c20a76cad2b033a8c16a5b01e2dc9eb
Juno Kim <lippoint.surf0622@gmail.com>
2023-02-27 21:05:00 +0900

    relation to project-photo table

--- admin/src/infrastructure/entities/project/project.entity.ts
+++ admin/src/infrastructure/entities/project/project.entity.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ProjectPhoto } from '../project-photo/project-photo.entity';
 
 @Entity()
 export class Project {
@@ -6,13 +13,16 @@ export class Project {
   id: number;
 
   @Column({ unique: true })
-  projectname: string;
+  projectId: string;
 
   @Column()
   startdate: string;
 
   @Column({ nullable: true })
   enddate: string;
+
+  @OneToMany(() => ProjectPhoto, (photo) => photo.project)
+  images: ProjectPhoto[];
 
   @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
