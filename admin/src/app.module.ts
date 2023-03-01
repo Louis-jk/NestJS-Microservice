18177c682b5ebf62785caa2a8346adebf893cb4c
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-01 21:28:20 +0900

    import AuthModule

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -7,10 +7,16 @@ import { UserModule } from '@domain/user/user.module';
 import * as dotenv from 'dotenv';
 import { LoggerMiddleware } from '@middlewara/logger.middleware';
 import { config as TypeOrmCofig } from '@infrastructure/config/typeorm/typeorm.config';
+import { AuthModule } from '@domain/auth/auth.module';
 
 dotenv.config();
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmCofig), ProjectModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(TypeOrmCofig),
+    ProjectModule,
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
