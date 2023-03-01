594c2e9564957354cd33b66bffb5396310e292e1
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-01 21:36:51 +0900

    add validateUser function

--- admin/src/domain/auth/auth.service.ts
+++ admin/src/domain/auth/auth.service.ts
@@ -1,4 +1,11 @@
+import { UserService } from '@domain/user/user.service';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(email: string, password: number): Promise<any> {
+    const user = this.userService.findOne(email);
+  }
+}
