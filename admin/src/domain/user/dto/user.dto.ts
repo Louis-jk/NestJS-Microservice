6900b6f6d47cca90d43e11269da0a36268d6bec8
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-02 22:26:06 +0900

    modify updateUserDto interface Partial type -> extends class PartialType

--- admin/src/domain/user/dto/user.dto.ts
+++ admin/src/domain/user/dto/user.dto.ts
@@ -8,6 +8,7 @@ import {
 import { IUser } from '../interface/user.interface';
 import { CreateAddressDto } from './address.dto';
 import { Type } from 'class-transformer';
+import { PartialType } from '@nestjs/mapped-types';
 export class CreateUserDto implements IUser {
   @IsNotEmpty()
   username: string;
@@ -29,4 +30,4 @@ export class CreateUserDto implements IUser {
   address: CreateAddressDto;
 }
 
-export class UpdateUserDto implements Partial<IUser> {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
