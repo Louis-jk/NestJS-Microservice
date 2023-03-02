de92d943d6b00ee27961db1a330665e7a1de56df
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-02 13:30:10 +0900

    add imports and providers

--- admin/src/domain/auth/auth.module.ts
+++ admin/src/domain/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '@domain/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStarategy } from './local.strategy';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStarategy],
 })
 export class AuthModule {}
