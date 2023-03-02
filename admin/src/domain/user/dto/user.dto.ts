6fc78eb5f0fa19374e58ad556f020b6c80c0c73b
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-02 22:00:00 +0900

    add validate decoration

--- admin/src/domain/user/dto/user.dto.ts
+++ admin/src/domain/user/dto/user.dto.ts
@@ -1,8 +1,17 @@
+import { IsEmail, IsNotEmpty, IsNumberString } from 'class-validator';
 import { IUser } from '../interface/user.interface';
 export class CreateUserDto implements IUser {
+  @IsNotEmpty()
   username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNumberString()
   phone: string;
 }
 
