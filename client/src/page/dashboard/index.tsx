a328e06906b04cd0e59638114941a88670177fa9
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-20 19:44:04 +0900

    user data api test

--- client/src/page/dashboard/index.tsx
+++ client/src/page/dashboard/index.tsx
@@ -3,7 +3,13 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_HOST;
 
 const Dashboard = () => {
-  axios.get(`${BASE_URL}/api/v1/users`).then((resp) => console.log(resp));
+  const users = axios
+    .get(`${BASE_URL}/api/v1/users`)
+    .then((resp) => console.log(resp));
+
+  console.log('====================================');
+  console.log('users?', users);
+  console.log('====================================');
 
   return <h1>Dashboard</h1>;
 };
