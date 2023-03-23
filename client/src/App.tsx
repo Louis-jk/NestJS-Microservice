e961ac4bbb86ec1e5e378668646bcb11ad4793b3
Juno Kim <lippoint.surf0622@gmail.com>
2023-03-23 18:02:09 +0900

    add project route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Dashboard from './page/dashboard';
 import Main from './page/portfolio/main';
+import Project from './page/dashboard/project';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
   {
     path: '/dashboard',
     element: <Dashboard />,
+  },
+  {
+    path: '/dashboard/project',
+    element: <Project />,
   },
 ]);
 
